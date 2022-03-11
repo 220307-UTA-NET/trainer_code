b7172723d1698afd763ddccd38039fa61ba6d664
Richard Hawkins <Richard.Hawkins@revature.com>
2022-03-11 16:47:14 -0500

    Add FirstDotnet project

---
+++ wk-1/Dotnet/FirstDotnet/Program.cs
@@ -0,0 +1,52 @@
+
+string variable = "hi Amin!";
+
+//Console.WriteLine("this is our program!");
+//Console.WriteLine(variable);
+
+//variable = "let's reuse this variable";
+//Console.WriteLine(variable);
+
+//variable = Console.ReadLine();
+//Console.WriteLine(variable);
+
+int number = 5;
+float number_float = 5.789986F;
+//float is short for a floating-point value
+double number_double = 1.2346;
+
+Console.WriteLine("This is an int: " + number);
+Console.WriteLine("This is a float: " + number_float);
+Console.WriteLine("This is a double: " + number_double);
+
+Console.WriteLine("Let's add strings! " + variable );
+
+int x, y, z;
+x = 2;
+y = 3; 
+z = 4;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+z = x + y;
+
+Console.WriteLine(z);
+
+//order of operations PEMDAS (), ^, *, /, +, - PEMDAS first, then left to right!
+
+if ( z > y )
+{
+	Console.WriteLine("z is greater than y");
+}
+else if ( z < y )
+{
+	Console.WriteLine("z is less than y");
+}
+else
+{
+	Console.WriteLine("z is equal to y");
+}
+
+
