89079516a367213178e3fcbc6be3d0f321384c62
Richard Hawkins <Richard.Hawkins@revature.com>
2022-03-15 12:18:03 -0400

    update Guessing Game with Addition Challenge

--- wk-2/GuessingGame/GuessingGame/Program.cs
+++ wk-2/GuessingGame/GuessingGame/Program.cs
@@ -1,10 +1,12 @@
 //Guessing game - play a quick guessing game with the computer.
 Console.WriteLine("Welcome to the Guessing Game!");
+var rand = new Random();
 
 while ( true )
 {
 	Console.WriteLine("Enter the number for the menu option of your choice: ");
 	Console.WriteLine("[1] - Play the guessing game");
+	Console.WriteLine("[2] - Math Challenge!");
 	Console.WriteLine("[0] - Exit");
 
 	int menu = int.Parse(Console.ReadLine());
@@ -12,10 +14,13 @@ while ( true )
 	switch ( menu )
 	{
 		case 0:
+			Console.WriteLine("Press Enter to continue.");
+			Console.ReadLine();				
+			Console.Clear();
 			return;
 	
 		case 1:
-			﻿var rand = new Random(); //Creates the random number
+			﻿// var rand = new Random(); //Creates the random number
 			int secret = rand.Next(11); //Limit the random number from 0-10, then save as int to "secret"
 	
 			while ( true ) //Begin the loop!
@@ -27,25 +32,78 @@ while ( true )
 				if ( input == secret ) //If the user guessed the secret number (if input is equal to secret)
 				{
 					Console.WriteLine("Congratulations! You've correctly guessed the secret number!");
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
 					break; //break out of the while loop!
 				}
 	
 				else if ( input > secret ) //If the user guessed too high (if input is greater than secret)
 				{
 					Console.WriteLine("Uh oh, you've guessed too high!");
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
 				}
 	
 				else //Else (the only possible option left is that the user entered a number smaller than secret)
 				{
 					Console.WriteLine("Mhh, looks like you've guessed too low.");
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
 				}
 			}
 	
 			Console.WriteLine("Good game, thanks for playing!");
+			Console.WriteLine("Press Enter to continue.");
+			Console.ReadLine();
+			Console.Clear();
 			break;
 	
+		case 2:
+			// var rand = new Random(); // Generating the random values for the problem
+			int num1 = rand.Next(101);
+			int num2 = rand.Next(101);
+
+			int solution = num1 + num2; // Findng the correct solution
+
+			Console.WriteLine("Welcome to the Addition Challenge!");
+			
+			while ( true )
+			{
+				Console.WriteLine("Your question is: ");
+				Console.WriteLine( num1 + " + " + num2 + " = ?"); // "33 + 45"
+
+				Console.WriteLine("Please enter your solution: ");
+				int userGuess = int.Parse(Console.ReadLine()); // Accepting the users solution
+
+				if ( solution == userGuess )
+				{
+					Console.WriteLine("You got it!");
+
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
+
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Not quite, you were off by: " + Math.Abs( solution - userGuess ));
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
+				}
+			}
+
+			break;
+
 		default:
 			Console.WriteLine("Bad input: Input was not a valid option.");
+			Console.WriteLine("Press Enter to continue.");
+			Console.ReadLine();
+			Console.Clear();
 			break;
 	}
 }
