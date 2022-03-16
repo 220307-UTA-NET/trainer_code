cd3238f32fd1b84434a5fd6bdc3ee31719e76f74
Richard Hawkins <Richard.Hawkins@revature.com>
2022-03-16 16:55:07 -0400

    Add data structure and collection notes

---
+++ wk-2/DataStructures/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] arrayName = { "Kevin", "Dan", "Anu", "Kelly", "Trygg" };
+
+string Nametwo = arrayName[ 2 ];
+arrayName[ 1 ] = "Cam";
+
+Array.Sort(arrayName);
+Array.Reverse(arrayName);
+
+//Enumerables
+foreach ( string name in arrayName )
+{
+	Console.WriteLine( name );
+}
+
+int[] myNumbers = {3,9,12,5,10,23,7,14};
+
+Array.Sort(myNumbers);
+
+foreach ( int i in myNumbers )
+{
+    Console.WriteLine(i);
+}
