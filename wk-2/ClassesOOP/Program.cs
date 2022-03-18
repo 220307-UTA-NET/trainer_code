59505c37c64498a605c673b8cbbcedcfb447043c
Richard Hawkins <Richard.Hawkins@revature.com>
2022-03-18 15:57:43 -0400

    Square Class Practice

---
+++ wk-2/ClassesOOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ClassesOOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            // The program starts here...
+            Square squareOne = new Square();
+
+            Console.WriteLine(squareOne.numberOfSides);
+            Console.WriteLine(squareOne.getPerimiter());
+        }
+    }
+}
