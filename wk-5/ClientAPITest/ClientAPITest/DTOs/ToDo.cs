58eb21ecd75bb403ac117e5f25512cb68a9052b4
Richard Hawkins <Richard.Hawkins@revature.com>
2022-04-07 16:01:54 -0400

    Adding ClientAPITest

---
+++ wk-5/ClientAPITest/ClientAPITest/DTOs/ToDo.cs
@@ -0,0 +1,20 @@
+﻿namespace ClientAPITest.DTOs
+{
+    // DTOs  (Data Transfer Objects) 
+    // aren't meant to model a behavior
+    // just the values of an object
+
+
+    //When we start to use a client application to communicate with a server,
+    // the Server cannot use Console.Read or Console.Write, so all User Interaction 
+    // must come through the Console Application (the client application).
+
+    public class ToDo
+    {
+        // Fields
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+}
---
+++ wk-5/ClientAPITest/ClientAPITest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ClientAPITest
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            HttpClient client = new HttpClient();
+
+            string response = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+
+            List<DTOs.ToDo> todo = JsonSerializer.Deserialize<List<DTOs.ToDo>>(response);
+
+            //Console.WriteLine(response);
+            foreach( var item in todo)
+            {
+                Console.WriteLine(item.title);
+            }
+            
+        }
+    }
+}
