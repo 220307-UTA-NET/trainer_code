4250f33dab1382da7a3b28f57b0feebbdae96248
Richard Hawkins <Richard.Hawkins@revature.com>
2022-04-11 17:18:07 -0400

    update DemoApp.ConApp

---
+++ wk-6/DemoApp/DemoApp.Console/DemoApp.UI/DTOs/DeviceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoApp.UI.DTOs
+{
+    public class DeviceDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public int OS { get; set; }
+    }
+}
---
+++ wk-6/DemoApp/DemoApp.Console/DemoApp.UI/IO.cs
@@ -0,0 +1,117 @@
+﻿using DemoApp.UI.DTOs;
+using System.Net.Http.Json;
+using System.Net.Mime;
+
+namespace DemoApp.UI
+{
+    public class IO
+    {
+        // Fields
+        private readonly Uri uri;
+
+        public static readonly HttpClient httpClient = new HttpClient();
+
+        // Constructors
+        public IO (Uri uri)
+        {
+            this.uri = uri;
+        }
+
+
+        // Methods
+        public async Task BeginAsync()
+        {
+            Console.WriteLine("ConsoleApp Running...");
+            bool loop = true;
+
+            do
+            {
+                int choice = MainMenu();
+                switch(choice)
+                {
+                    case -1:
+                        Console.WriteLine("Bad input, please try again.");
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadLine();
+                        break;
+                    case 0:
+                        loop = false; break;
+                    case 1:
+                        await DisplayAllDevicesAsync();
+                        break;
+                }
+            }while (loop == true);
+        }
+
+        private int MainMenu()
+        {
+            Console.Clear();
+            int choice = -1;
+            Console.WriteLine("Please select the option of your choice:");
+            Console.WriteLine("[0] - Exit");
+            Console.WriteLine("[1] - Get All Devices Information");
+            Console.WriteLine("[2] - Get A Devices Information");
+            string? input = Console.ReadLine();
+            Console.Clear();
+            //if (int.TryParse(input, out choice))
+            //{
+            //    return choice;
+            //}
+            //else
+            //{
+            //    choice = -1;
+            //    return choice;
+            //}
+            if (!int.TryParse(input, out choice))
+            { choice = -1; }
+            return choice;
+        }
+        
+        private async Task DisplayAllDevicesAsync()
+        {   
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri.ToString() + "Devices");
+            request.Headers.Accept.Add(new(MediaTypeNames.Application.Json));
+
+            //try
+            //{
+            //    response = await httpClient.SendAsync(request);
+
+            //}
+            //catch (HttpRequestException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine("Press any key to continue.");
+            //    Console.ReadLine();
+            //}
+
+            using (HttpResponseMessage response = await httpClient.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+
+                if (response.Content.Headers.ContentType?.MediaType != MediaTypeNames.Application.Json)
+                {
+                    throw new ArrayTypeMismatchException();
+                }
+
+                var devices = await response.Content.ReadFromJsonAsync<List<DeviceDTO>>();
+
+                if (devices != null)
+                {
+                    Console.WriteLine("Devices: ");
+                    foreach (var device in devices)
+                    {
+                        Console.WriteLine("Device Name: " + device.Name);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No devices found.");
+                   
+                }
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
