53b7af87df0c94f056f7ccfafd4e5fc6915119bb
Richard Hawkins <Richard.Hawkins@revature.com>
2022-04-13 16:25:09 -0400

    update for Azure Deployment

---
+++ wk-6/DemoApp/DemoApp.Api/DemoApp.Api/Controllers/CountController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountController : ControllerBase
+    {
+        // Fields
+
+        static private int count = 0;
+        private readonly ILogger<CountController> _logger;
+
+
+        // Constructor
+        
+        public CountController(ILogger<CountController> logger)
+        {
+            this._logger = logger;
+        }
+
+        // Methods
+
+        // every time we call the Get method, i want to return the number of times the methd has been called.
+        // GET: api/CountController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            count++;
+            _logger.LogInformation("Count.Get executed. Count: {count}", count);
+            return new string[] { count.ToString() };
+        }
+
+        // GET api/<CountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/<CountController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        // PUT api/<CountController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/<CountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- wk-6/DemoApp/DemoApp.Api/DemoApp.Api/Program.cs
+++ wk-6/DemoApp/DemoApp.Api/DemoApp.Api/Program.cs
@@ -5,13 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Connection String:
 // Argument, Hardcoded, File, User Secrets, Environmental Variables
-string connectionString = builder.Configuration["connectionString"];
+string connectionString = builder.Configuration.GetConnectionString("connectionString");
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddSingleton<IRepository>(sp => new SQLRepository(connectionString, sp.GetRequiredService<ILogger<SQLRepository>>()));
 
 var app = builder.Build();
